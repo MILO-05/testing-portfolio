4dedae3bd04e7e2cd1f0b426c3754f00b4d87b48
MILO-05 <diegoradityas11@gmail.com>
2025-07-01 21:08:56 +0700

    Update

--- api/index.py
+++ api/index.py
@@ -1,3 +1,9 @@
-from app import app
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template_string("<h1>Hello from Flask on Vercel!</h1>")
 
 handler = app.wsgi_app
